494a4c71d7c5955e577f72ed739169240702c3ed
Christian Villa <christianaraujovilla@gmail.com>
2025-08-19 12:05:16 -0300

    Adicionando exercício de calculo ao quadrado e cubo

---
+++ numero-ao-quadrado-e-cubo/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> //biblioteca para operações matemáticas
+
+
+// RECEBE UM NÚMERO DO USUARIO IMPRIME SEU QUADRADO E SEU CUBO
+
+/*
+int main ()
+{
+    double num, quadrado, cubo;
+    printf("Informe um numero:");
+    scanf("%lf", &num);
+    quadrado = pow (num, 2);  // é só declarar o número, elevado a 2 de quadrado e 3 de cubo;
+    cubo = pow(num, 3);
+    printf("O numero ao quadrado eh: %.0f\n", quadrado);
+    printf("O numero ao cubo eh: %.0f", cubo);
+    return 0;
+}
+*/
