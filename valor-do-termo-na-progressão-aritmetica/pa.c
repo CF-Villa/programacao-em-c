ab34f3233e9852f6a377d1ebf55358b045d0aa66
Christian Villa <christianaraujovilla@gmail.com>
2025-08-20 20:56:24 -0300

    Add files via upload

---
+++ valor-do-termo-na-progressão-aritmetica/pa.c
@@ -0,0 +1,17 @@
+/*Nesse exercício, você deve escrever um algoritmo para calcular o valor de um termo n de uma
+progressão aritméNca (PA), que tem a seguinte fórmula:
+an= "#$%&$#' + ) − 1 ∗ #-.ã'
+Dessa forma, o algoritmo deve solicitar ao usuário que informe o valor do primeiro termo, o valor
+da razão da P.A. e seu número de ordem (n), e deve imprimir o termo calculado a parNr dos
+valores informados.*/
+
+pede primeiro termo
+
+pede razão não informada
+
+faz calculo
+
+
+** ta no slide de 20/08/2025
+
+
